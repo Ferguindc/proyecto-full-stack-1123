1e01a8a079a3371a3e18d403975e6b2093ae4850
CETECOM <alv.delcanto@duocuc.cl>
2025-05-07 15:40:18 -0400

    repository

--- src/main/java/grupo2/proyecto/full/stack1/Repository/ProductRepository.java
+++ src/main/java/grupo2/proyecto/full/stack1/Repository/ProductRepository.java
@@ -4,6 +4,7 @@ import grupo2.proyecto.full.stack1.Modelo.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
